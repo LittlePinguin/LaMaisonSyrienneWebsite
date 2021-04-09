79813f0a8f0606504b389a74e735eb45257c12be
samibk201 <45363113+samibk201@users.noreply.github.com>
2021-04-09 18:28:21 +0200

    Add files via upload

--- Restaurant/JS/main.js
+++ Restaurant/JS/main.js
@@ -1,15 +1,47 @@
 $(document).ready(function(){
     //$(this).scrollTop(0);
 
+    function detectMob() {
+        const toMatch = [
+            /Android/i,
+            /webOS/i,
+            /iPhone/i,
+            /iPad/i,
+            /iPod/i,
+            /BlackBerry/i,
+            /Windows Phone/i
+        ];
+
+        return toMatch.some((toMatchItem) => {
+            return navigator.userAgent.match(toMatchItem);
+        });
+    }
+
+    console.log(detectMob());
+
+
+    if(!detectMob()){
+        console.log("here");
+        $("body").css({
+            "min-width":"717px"
+        });
+    }else{
+
+        console.log("here2");
+    }
+
+
     var size = $(window).width();
+
+
     //
     // $('h1').css({
     //     "font-size":"40px"
     // });
 
-    $('[data-spy="scroll"]').on('activate.bs.scrollspy', function () {
-        $(this).addClass('active');
-    })
+    // $('[data-spy="scroll"]').on('activate.bs.scrollspy', function () {
+    //     $(this).addClass('active');
+    // })
 
     $('.nav-link').removeClass('active');
     $('.nav-link').on('click', function(){
@@ -32,13 +64,16 @@ $(document).ready(function(){
             "display":"none"
         });
     });
+
+
     $('.menu .item').not('.assiettes').hide();
+
     $('.control .btn').click(function (){
        $(this).addClass('btn-active').siblings().removeClass('btn-active');
 
        let filter = $(this).attr('data-filter');
-       $('.menu .item').not('.'+filter).hide(200);
-       $('.menu .item').filter('.'+filter).show(400);
+       $('.menu .item').not('.'+filter).hide(100);
+       $('.menu .item').filter('.'+filter).show(200);
     });
 
     //onResize
