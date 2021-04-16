956c392b0ec3cd8ae9a5c8fecca17a92e0d8988a
BELAID Amani <58103824+LittlePinguin@users.noreply.github.com>
2021-04-16 13:14:46 +0200

    Add files via upload

--- Restaurant/JS/main.js
+++ Restaurant/JS/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    //$(this).scrollTop(0);
 
     function detectMob() {
         const toMatch = [
@@ -21,11 +22,10 @@ $(document).ready(function(){
     if(!detectMob()){
         console.log("here");
         $("body").css({
-            "min-width":"990px"
+            "min-width":"717px"
         });
 
     }else{
-        document.getElementById('MyStyle').setAttribute('href', 'css/PhoneSheet.css');
         $('.column > p > a').css({
             "font-size" : "2.2em"
         });
@@ -35,6 +35,22 @@ $(document).ready(function(){
     var size = $(window).width();
 
 
+    //
+    // $('h1').css({
+    //     "font-size":"40px"
+    // });
+
+    // $('[data-spy="scroll"]').on('activate.bs.scrollspy', function () {
+    //     $(this).addClass('active');
+    // })
+
+    $('.nav-link').removeClass('active');
+    $('.nav-link').on('click', function(){
+        $('.nav-link').each(function(){
+            $(this).removeClass('active');
+        });
+        $(this).addClass('active');
+    });
 
     $('.navbar-toggler').focus(function(){
         $('.nav-tabs').removeClass("tab-btm");
@@ -50,53 +66,29 @@ $(document).ready(function(){
         });
     });
 
-    // menu onglets handler
+
     $('.menu .item').not('.assiettes').hide();
-    AOS.refresh();
 
     $('.control .btn').click(function (){
-        var filter = $(this).attr('data-filter');
+       $(this).addClass('btn-active').siblings().removeClass('btn-active');
 
-        $(this).addClass('btn-active').siblings().removeClass('btn-active');
-
-        $('.menu .item').not('.'+filter).fadeOut(200);
-        $('.menu .item').filter('.'+filter).fadeIn(400);
+       let filter = $(this).attr('data-filter');
+       $('.menu .item').not('.'+filter).fadeOut(200);
+       $('.menu .item').filter('.'+filter).fadeIn(400);
     });
 
     //onResize
     $(window).resize(function(){
         w_w = $(window).width();
-        if(w_w > 990){
+        if(w_w > 991){
             $(".nav-tabs").css({
                 "display":"flex"
             });
         }
-    });
-
-    // scroll to top button
-    $(window).on("scroll", function() {
-        var scrollPos = $(window).scrollTop();
-        if (scrollPos <= 0) {
-            $(".scroll-top-icon").fadeOut();
-        } else {
-            $(".scroll-top-icon").fadeIn();
+        if(w_w < 992){
+            $(".nav-tabs").css({
+                "display":"none"
+            });
         }
     });
-
-    // navbar disapear when scroll down + 
-    // come back when scroll up
-    var prevScrollpos = window.pageYOffset;
-    window.onscroll = function() {
-      var currentScrollPos = window.pageYOffset;
-      if (prevScrollpos > currentScrollPos) {
-        $(".navbar").css({
-            "top":"0"
-        });
-      } else {
-        $(".navbar").css({
-            "top":"-100px"
-        });
-      }
-      prevScrollpos = currentScrollPos;
-    }
 });
